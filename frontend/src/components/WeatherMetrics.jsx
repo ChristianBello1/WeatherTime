7fbe3e86c1429b85a4a1bc39d1a7678ae8a44c20
Christianbello1 <lulabchannel2@gmail.com>
2024-09-21 20:53:03 +0200

    icone in public

--- frontend/src/components/WeatherMetrics.jsx
+++ frontend/src/components/WeatherMetrics.jsx
@@ -8,32 +8,32 @@ function WeatherMetrics({ metrics }) {
   return (
     <div className="weather-metrics">
       <div className="metric">
-        <img src="./src/assets/sun.svg" alt="UV Index" className="metric-icon" />
+        <img src="./public/assets/sun.svg" alt="UV Index" className="metric-icon" />
         <p>UV Index</p>
         <span>{uvIndex}</span>
       </div>
       <div className="metric">
-        <img src="./src/assets/thermometer.svg" alt="Apparent Temperature" className="metric-icon" />
+        <img src="./public/assets/thermometer.svg" alt="Apparent Temperature" className="metric-icon" />
         <p>Apparent Temperature</p>
         <span>{Math.round(apparentTemperature)}°C</span>
       </div>
       <div className="metric">
-        <img src="./src/assets/droplet.svg" alt="Humidity" className="metric-icon" />
+        <img src="./public/assets/droplet.svg" alt="Humidity" className="metric-icon" />
         <p>Humidity</p>
         <span>{Math.round(relativeHumidity)}%</span>
       </div>
       <div className="metric">
-        <img src="./src/assets/wind.svg" alt="Wind Speed" className="metric-icon" />
+        <img src="./public/assets/wind.svg" alt="Wind Speed" className="metric-icon" />
         <p>Wind</p>
         <span>{Math.round(windSpeed)} km/h</span>
       </div>
       <div className="metric">
-        <img src="./src/assets/arrow-down.svg" alt="Pressure" className="metric-icon" />
+        <img src="./public/assets/arrow-down.svg" alt="Pressure" className="metric-icon" />
         <p>Atmospheric Pressure</p>
         <span>{Math.round(surfacePressure)} hPa</span>
       </div>
       <div className="metric">
-        <img src="./src/assets/eye.svg" alt="Visibility" className="metric-icon" />
+        <img src="./public/assets/eye.svg" alt="Visibility" className="metric-icon" />
         <p>Visibility</p>
         <span>{Math.round(visibility)} km</span>
       </div>
